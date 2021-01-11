00b43707171625c7c24c2391e55777e617b2b30e
Joao Victor Ramires <joaoramires.engsoft@gmail.com>
2021-01-11 04:30:45 -0300

    Expose headers

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,6 +4,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
     resource '*',
              headers: :any,
-             methods: %i[get post put patch delete]
+             methods: %i[get post put patch delete],
+             expose: %w[access-token client expiry token-type uid]
   end
 end
