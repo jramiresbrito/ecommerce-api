72ad72aa2572a4203142ab0086239c12bbd89507
Joao Victor Ramires <joaoramires.engsoft@gmail.com>
2021-03-01 09:54:25 -0300

    Update model tests to use Like Searchable

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -18,6 +18,6 @@ RSpec.describe Product, type: :model do
   it { is_expected.to have_many(:product_categories).dependent(:destroy) }
   it { is_expected.to have_many(:categories).through(:product_categories) }
 
-  it_behaves_like 'name searchable concern', :product
+  it_behaves_like 'like searchable concern', :product, :name
   it_behaves_like 'paginatable concern', :product
 end
