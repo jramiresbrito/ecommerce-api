a95f3ba71228adebc4d68ab21703b56854589e4a
Joao Victor Ramires <joaoramires.engsoft@gmail.com>
2021-03-01 09:34:58 -0300

    License model

---
+++ app/models/license.rb
@@ -0,0 +1,9 @@
+class License < ApplicationRecord
+  belongs_to :game
+
+  validates :key, :platform, :status, presence: true
+  validates :key, uniqueness: { case_sensitive: false, scope: :platform }
+
+  enum platform: { steam: 1, battle_net: 2, origin: 3 }
+  enum status: { available: 1, in_use: 2, inactive: 3 }
+end
